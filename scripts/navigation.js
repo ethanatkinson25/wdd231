fa75f12ab9c08933de4b8b38175dd28870762e08
ethanatkinson <ethanatkinson11@icloud.com>
2025-10-29 10:14:03 -0600

    Finished week 01 learning activities

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,15 @@
 const navButton = document.querySelector('#hamburger-button');
+const navBar = document.querySelector('#nav-bar');
+const currentYear = new Date().getFullYear();
+const copyright = document.querySelector('#copyright');
+const lastModified = new Date(document.lastModified);
+const lastModifiedText = document.querySelector('#last-modified');
+
+copyright.textContent = `\u00A9 | ${currentYear}`;
 
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
 });
+
+lastModifiedText.textContent = `Last Modified: ${lastModified}`;

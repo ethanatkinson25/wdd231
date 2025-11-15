c49d843b2f761124a82e6f2799a444c6dd81fc59
ethanatkinson <ethanatkinson11@icloud.com>
2025-11-15 11:30:41 -0700

    Completed forecast section

--- chamber/scripts/weather.mjs
+++ chamber/scripts/weather.mjs
@@ -1,6 +1,5 @@
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=41.140076825789784&lon=-112.06430780658653&appid=4529d3124849a34fdee49a25a29734a1&units=imperial";
 const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=41.140076825789784&lon=-112.06430780658653&appid=4529d3124849a34fdee49a25a29734a1&units=imperial";
-//https://api.openweathermap.org/data/2.5/forecast?lat=41.140076825789784&lon=-112.06430780658653&appid=4529d3124849a34fdee49a25a29734a1&units=imperial
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
@@ -13,9 +12,8 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             const forecastData = await forecastResponse.json();
-            console.log(data);
-            console.log(forecastData);
             displayResults(data);
+            displayForecast(forecastData);
         } else {
             throw Error(await response.text());
         }
@@ -25,14 +23,35 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-    console.log("hello");
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', 'weather icon');
     captionDesc.textContent = `${data.weather[0].description} `;
+}
 
-    // forecast.innerHTML = nextThree.map(day => { })
+function displayForecast(data) {
+    const nextThree = data.list.filter(forecast => forecast.dt_txt.includes('18:00:00'));
+    forecast.innerHTML = '';
+    console.log(nextThree);
+
+    for (let i = 0; i < nextThree.length; i++) {
+        const item = nextThree[i];
+        const date = new Date(item.dt_txt);
+        const iconsrc = `https://openweathermap.org/img/w/${item.weather[0].icon}.png`;
+        const description = item.weather[0].description;
+
+        const temp = `${item.main.temp.toFixed(0)}&deg;F`;
+
+        forecast.innerHTML += `
+            <div class="forecast-day">
+                <h3>${date}</h3>
+                <figure><img src="${iconsrc}" alt="${description}" width="75" height="75"></figure>
+                <figcaption>${description}</figcaption>
+                <p>${temp}</p>
+            </div>
+        `;
+    }
 }
 
 apiFetch();

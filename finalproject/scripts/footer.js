ce1df74b482e6d811ffa248211d133722e8e2ced
threadripper <ethanatkinson11@icloud.com>
2025-12-01 10:16:43 -0700

    Added unoptimized images

---
+++ finalproject/scripts/footer.js
@@ -0,0 +1,13 @@
+const currentYear = new Date().getFullYear();
+const copyright = document.querySelector('#copyright');
+const lastModified = new Date(document.lastModified);
+const lastModifiedText = document.querySelector('#last-modified');
+
+copyright.textContent = `\u00A9 | Ethan Atkinson |  WDD231 | ${currentYear}`;
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
+
+lastModifiedText.textContent = `Last Modified: ${lastModified}`;

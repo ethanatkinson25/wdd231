cdcd087a2f6cced6772a5f9a818717ff8a25b6c6
threadripper <ethanatkinson11@icloud.com>
2025-12-02 10:10:36 -0700

    Added nav bar structure

--- finalproject/scripts/footer.js
+++ finalproject/scripts/footer.js
@@ -2,6 +2,8 @@ const currentYear = new Date().getFullYear();
 const copyright = document.querySelector('#copyright');
 const lastModified = new Date(document.lastModified);
 const lastModifiedText = document.querySelector('#last-modified');
+const navButton = document.querySelector('#hamburger-button');
+const navBar = document.querySelector('#nav-bar');
 
 copyright.textContent = `\u00A9 | Ethan Atkinson |  WDD231 | ${currentYear}`;
 
